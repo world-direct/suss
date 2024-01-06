f62d80d3c8e5c930c04898a8c11b2ccee4db93d9
gprossliner <6724584+gprossliner@users.noreply.github.com>
2024-01-06 18:03:09 +0100

    added script to be tested

---
+++ update.sh
@@ -0,0 +1,43 @@
+#/bin/sh
+
+set -ux
+
+function suss {  
+    echo "SUSS: $1"
+    curl --fail "http://localhost:9993/$1"
+
+    if [[ "$?" != "0" ]]; then
+        echo "SUSS call failed, stop script execution"
+        exit 1
+    fi 
+}
+
+# check for updates first
+yum check-update
+if [[ "$?" != "100" ]]; then
+    echo "No package updates available, exiting"
+    exit 0
+fi
+
+# synchronize with other hosts
+suss synchronize
+
+# teardown critical workload
+suss teardown
+
+# and finally run update
+yum update -y
+
+# check if reboot is requred
+needs-restarting -r
+if [[ "$?" == "130" ]]; then
+    echo "No reboot required, releasing lock"
+    suss release
+    exit 0
+fi
+
+# need reboot, release delayed
+suss releasedelayed
+
+# and finally reboot
+reboot
